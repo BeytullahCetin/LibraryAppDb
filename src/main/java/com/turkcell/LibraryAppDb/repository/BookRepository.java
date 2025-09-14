0ae9ba762341fba879ddad42c4286abf83e747f5
Ardach <coha17@itu.edu.tr>
2025-09-14 12:24:08 +0300

    BookRepository custom query

--- src/main/java/com/turkcell/LibraryAppDb/repository/BookRepository.java
+++ src/main/java/com/turkcell/LibraryAppDb/repository/BookRepository.java
@@ -1,9 +1,19 @@
 package com.turkcell.LibraryAppDb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.turkcell.LibraryAppDb.entity.Book;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+	@Query("select b from Book b where lower(b.title) like lower(concat('%', :keyword, '%'))")
+	List<Book> searchByTitle(@Param("keyword") String keyword);
+
+	@Query("select b from Book b where lower(b.language.name) = lower(:languageName)")
+	List<Book> findByLanguageName(@Param("languageName") String languageName);
 
 }
