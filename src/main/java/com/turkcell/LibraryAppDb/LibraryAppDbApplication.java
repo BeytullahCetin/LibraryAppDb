8882ee993d616468502a30125d2280abddc4a494
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2025-09-21 01:26:34 +0300

    Update LibraryAppDbApplication.java
    Co-Authored-By: Ardach <43939501+Ardach@users.noreply.github.com>

--- src/main/java/com/turkcell/LibraryAppDb/LibraryAppDbApplication.java
+++ src/main/java/com/turkcell/LibraryAppDb/LibraryAppDbApplication.java
@@ -6,44 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryAppDbApplication {
 
-	// TODO: 1
-	/*
-	 * - DTO - Mapper
-	 * BookCopy, Borrow(Alişan)
-	 * Fine, Reservation(Beytullah)
-	 * - REPOSITRY - Rules
-	 * Book, Borrow, Customer(Hakkı)
-	 * Reservation, Fine(Beytullah)
-	 * - SERVICE - Controller
-	 * Customer, Book, Borrow (Hakkı)
-	 * Reservation, Fine(Beytullah)
-	 */
-
-	// 2 entity için dto-service-controller
-	// yapısını koruyarak add ve getAll methodlarını tasarlamak.
-
-	// -------
-
-	// Ana tablolar için:
-	// Listeleme
-	// ID'ye göre getirme
-	// ekleme
-	// silme
-	// güncelleme
-	// CRUD
-
-	// -------
-
-	// En az 2 entity icin custom query yazilacak. (Plan bizde)
-	// Her istekte requestleri validasyon kurallariyla koruyalim.
-
-	// -------
-
-	// 1 tane servis'e automapper entegre et.
-
-	// --------
-
-	//
+	// TODO: Login/Signup işlemlerini ekle.
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryAppDbApplication.class, args);
