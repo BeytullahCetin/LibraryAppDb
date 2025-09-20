f493474dcfa0d4d6375c5f5e9d2b00b069581cc3
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2025-09-21 00:37:48 +0300

    Create GetByCustomerIdBorrowResponse.java
    Co-Authored-By: Ardach <43939501+Ardach@users.noreply.github.com>

---
+++ src/main/java/com/turkcell/LibraryAppDb/dto/borrow/response/GetByCustomerIdBorrowResponse.java
@@ -0,0 +1,73 @@
+package com.turkcell.LibraryAppDb.dto.borrow.response;
+
+import java.util.Date;
+
+public class GetByCustomerIdBorrowResponse {
+	private int id;
+	private Date borrDate;
+	private Date dueDate;
+	private Date returnDate;
+	private int customerId;
+	private int bookCopyId;
+
+	public GetByCustomerIdBorrowResponse() {
+	}
+
+	public GetByCustomerIdBorrowResponse(int id, Date borrDate, Date dueDate, Date returnDate, int customerId,
+			int bookCopyId) {
+		this.id = id;
+		this.borrDate = borrDate;
+		this.dueDate = dueDate;
+		this.returnDate = returnDate;
+		this.customerId = customerId;
+		this.bookCopyId = bookCopyId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public int getBookCopyId() {
+		return bookCopyId;
+	}
+
+	public void setBookCopyId(int bookCopyId) {
+		this.bookCopyId = bookCopyId;
+	}
+
+	public Date getBorrDate() {
+		return borrDate;
+	}
+
+	public void setBorrDate(Date borrDate) {
+		this.borrDate = borrDate;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+}
